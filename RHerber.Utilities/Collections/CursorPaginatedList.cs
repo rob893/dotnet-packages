e8d2de36ef13abc343c4779ad9a5def6628ac0b5
Robert Herber <rwherber@gmail.com>
2021-10-24 15:17:31 -0700

    feat: initial commit

---
+++ RHerber.Utilities/Collections/CursorPaginatedList.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RHerber.Utilities.Collections
+{
+    public sealed class CursorPaginatedList<TEntity, TEntityKey> : IEnumerable<TEntity>
+        where TEntity : class
+        where TEntityKey : IEquatable<TEntityKey>, IComparable<TEntityKey>
+    {
+        private readonly List<TEntity> items;
+
+        public CursorPaginatedList(ICollection<TEntity> items, bool hasNextPage, bool hasPreviousPage, string? startCursor, string? endCursor, int? totalCount)
+        {
+            if (items == null)
+            {
+                throw new ArgumentNullException(nameof(items));
+            }
+
+            this.HasNextPage = hasNextPage;
+            this.HasPreviousPage = hasPreviousPage;
+            this.StartCursor = startCursor;
+            this.EndCursor = endCursor;
+            this.TotalCount = totalCount;
+            this.PageCount = items.Count;
+            this.items = new List<TEntity>(items);
+        }
+
+        public bool HasNextPage { get; }
+
+        public bool HasPreviousPage { get; }
+
+        public string? StartCursor { get; }
+
+        public string? EndCursor { get; }
+
+        public int? TotalCount { get; }
+
+        public int PageCount { get; }
+
+        public IEnumerator<TEntity> GetEnumerator()
+        {
+            return this.items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.items.GetEnumerator();
+        }
+    }
+}
